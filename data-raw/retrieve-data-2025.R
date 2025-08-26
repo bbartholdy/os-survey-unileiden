e69fc46d2625d9218116172dccd400f922ba59a1
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-08-26 21:31:50 +0200

    add files to retrieve data and GitHub workflows

---
+++ data-raw/retrieve-data-2025.R
@@ -0,0 +1,47 @@
+## Script will not run as it requires a Qualtrics access token ##
+
+# Load the httr2 library
+library(qualtRics)
+library(dplyr)
+
+# Retrieve survey --------------------------------------------------------
+
+qualtrics_api_credentials(
+  api_key = Sys.getenv("QUALTRICS_TOKEN"),
+  base_url = "fra1.qualtrics.com"
+)
+
+responses_raw <- fetch_survey(
+  surveyID = Sys.getenv("QUALTRICS_SURVEY"),
+  include_display_order = FALSE
+)
+
+# Data cleaning ----------------------------------------------------------
+
+responses_raw$wave <- 2025
+
+questions <- survey_questions(surveyID = Sys.getenv("QUALTRICS_SURVEY"))
+questions <- questions |>
+  mutate(
+    question = remove_html(question),
+    options = NA
+  )
+
+if(!file.exists("data/2025_questions.csv")) readr::write_csv(questions, "data/2025_questions.csv")
+
+half_full <- responses_raw |>
+  mutate(
+    Progress = as.numeric(Progress),
+    Duration = as.numeric(`Duration (in seconds)`)
+  ) |>
+  mutate(across(
+    where(is.character),
+    ~ stringi::stri_replace(., regex = "^Other, namely:.*", "Other")
+  )) |>
+  filter(
+    Q29 == "Yes (continue to questions)", # include only consenting respondents
+    Progress > 50 # include only surveys 50%+ completed
+  ) |>
+  select(!c(StartDate, EndDate, Status, RecordedDate, DistributionChannel, UserLanguage, Q29, `Duration (in seconds)`)) # deselect columns that aren't needed
+
+readr::write_csv(half_full, here::here("data/2025_responses-wide.csv"))
