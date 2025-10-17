22f882da0de5231dae0eef7427be0daf570b6d69
bbartholdy <47455812+bbartholdy@users.noreply.github.com>
2025-10-17 22:01:26 +0200

    add response options to About section

--- data-raw/retrieve-data-2025.R
+++ data-raw/retrieve-data-2025.R
@@ -27,7 +27,18 @@ questions <- questions |>
     options = NA
   )
 
-if(!file.exists("data/2025_questions.csv")) readr::write_csv(questions, "data/2025_questions.csv")
+# extract the response options and collapse to a single line
+options <- extract_colmap(responses_raw) |>
+  mutate(options = stringi::stri_extract(description, regex = "(?<=Selected\\sChoice\\s-\\s).*")) |>
+  group_by(ImportId) |>
+  mutate(options = paste0(options, collapse = ";")) |>
+  ungroup() |>
+  distinct(ImportId, .keep_all = TRUE) |>
+  select(!qname) |>
+  right_join(questions, c("ImportId" = "qid"), ) |>
+  select(qname, question, options)
+
+if(!file.exists("data/2025_questions.csv")) readr::write_csv(options, "data/2025_questions.csv")
 
 half_full <- responses_raw |>
   mutate(
